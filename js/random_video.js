827ee8d65b67f6dbfa6af73548acf075f7bc208d
masajinobe-ef <priscilla.effects@gmail.com>
2023-12-29 12:20:09 +0500

    v2.5

---
+++ js/random_video.js
@@ -0,0 +1,14 @@
+// Array of video file names in the folder
+var videoFiles = [
+  "bg.webm"
+];
+
+// Function to get a random video file from the array
+function getRandomVideo() {
+  var randomIndex = Math.floor(Math.random() * videoFiles.length);
+  return "videos/" + videoFiles[randomIndex];
+}
+
+// Set the source of the video element to a random video file
+var videoElement = document.getElementById("background-video");
+videoElement.src = getRandomVideo();
