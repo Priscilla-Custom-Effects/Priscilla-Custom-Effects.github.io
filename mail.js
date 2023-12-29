29f21c0633f55bd5c1e513f57cddf3730c857061
masajinobe-ef <priscilla.effects@gmail.com>
2023-12-29 10:35:02 +0500

    v2.4

--- mail.js
+++ mail.js
@@ -1,12 +1,24 @@
 function submitMail() {
-    var form = document.getElementById("mail");
+  var form = document.getElementById("mail");
 
-    var fio = form.elements["fio"].value;
-    var phone = form.elements["phone"].value;
-    var telegram = form.elements["telegram"].value;
-    var comment = form.elements["comment"].value;
+  var fio = form.elements["fio"].value;
+  var phone = form.elements["phone"].value;
+  var telegram = form.elements["telegram"].value;
+  var product = form.elements["product"].value;
+  var comment = form.elements["comment"].value;
 
-    var body = "ФИО: " + fio + "%0AТелефон: " + phone + "%0ATelegram: " + telegram + "%0AДополнительный комментарий: " + comment;
+  var body =
+    "ФИО: " +
+    fio +
+    "%0AТелефон: " +
+    phone +
+    "%0ATelegram: " +
+    telegram +
+    "%0AПродукты в заказе: " +
+    product +
+    "%0AДополнительный комментарий: " +
+    comment;
 
-    window.location.href = "mailto:priscilla.effects@gmail.com?subject=Заявка&body=" + body;
+  window.location.href =
+    "mailto:priscilla.effects@gmail.com?subject=Заявка&body=" + body;
 }
