630cc9ded6f7412a80e477a2ffcf9ca614ecc6a5
masajinobe-ef <priscilla.effects@gmail.com>
2023-12-29 13:54:09 +0500

    v2.8

--- js/random_video.js
+++ js/random_video.js
@@ -1,14 +1,21 @@
 // Array of video file names in the folder
-var videoFiles = [
-  "bg1.webm"
-];
+var videoFiles = ["bg1.webm"];
 
-// Function to get a random video file from the array
-function getRandomVideo() {
-  var randomIndex = Math.floor(Math.random() * videoFiles.length);
-  return "videos/" + videoFiles[randomIndex];
+// Function to get a random starting time for the video
+function getRandomVideoTime() {
+  // Set the maximum duration of the video in seconds
+  var maxDuration = 13; // Adjust this value based on your video's actual duration
+  // Generate a random starting time between 0 and maxDuration
+  var randomTime = Math.floor(Math.random() * maxDuration);
+  return randomTime;
 }
 
-// Set the source of the video element to a random video file
+// Set the source of the video element to the specified video file
 var videoElement = document.getElementById("background-video");
-videoElement.src = getRandomVideo();
+videoElement.src = "videos/" + videoFiles[0];
+
+// Set the random starting time for the video
+videoElement.addEventListener("loadedmetadata", function () {
+  // Wait for metadata to be loaded before setting currentTime
+  videoElement.currentTime = getRandomVideoTime();
+});
